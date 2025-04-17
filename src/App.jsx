5dc1464282e3fa152a4d88a86017ccf678033fe5
jatin-rajput563 <jkt3298@gmail.com>
2025-04-17 14:55:39 +0530

    starting-section-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './components/Header'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Instant from './components/Instant';
 function App() {
 
   return (
     <>
       <Header />
+      <Instant />
     </>
   )
 }
---
+++ src/components/Instant.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Instant = () => {
+    return (
+        <>
+            <section className='min-vh-100'>
+                <div className="container max-w-1060">
+                    
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Instant
