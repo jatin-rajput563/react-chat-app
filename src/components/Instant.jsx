ec9a302f9e77bfe46b52f328472bb34ffcd132a5
jatin-rajput563 <jkt3298@gmail.com>
2025-04-17 15:17:45 +0530

    instant-section-complete

--- src/components/Instant.jsx
+++ src/components/Instant.jsx
@@ -3,9 +3,11 @@ import React from 'react'
 const Instant = () => {
     return (
         <>
-            <section className='min-vh-100'>
+            <section className='py-100'>
                 <div className="container max-w-1060">
-                    
+                    <h2 className='instant-heading fw-medium'>Instant Team Chats</h2>
+                    <p className='instant-para fw-normal max-w-566 mx-auto m-0 pt-30'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa.</p>
+                    <img className='w-100 pt-60' src="./src/assets/png/instant-main-img.png" alt="instant-main-img" />
                 </div>
             </section>
         </>
