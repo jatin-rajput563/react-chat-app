0bc6a1d45bfb1d16e7200f29e8fd5fa2450cccc5
jatin-rajput563 <jkt3298@gmail.com>
2025-04-17 14:49:39 +0530

    hearo-section-complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Header from './components/Header'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
 
   return (
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,23 @@ import React from 'react'
 const Header = () => {
     return (
         <>
-        
+            <section className='min-vh-100 bg-blue'>
+                <div className="container max-w-1160">
+                    <div className="row">
+                        <div className="col-12 col-lg-6">
+                            <h1 className='fw-semibold header-heading text-white text-center text-lg-start'>Have your best chat</h1>
+                            <p className='header-para fw-normal text-white pt-20 m-0 text-center text-lg-start mx-auto mx-lg-0'>Fast, easy & unlimited team chat.</p>
+                            <div className='mt-60 d-flex gap-36 justify-content-center justify-content-lg-start mb-30 flex-wrap'>
+                                <button className='header-try-btn fw-medium bg-white'>Try it Free</button>
+                                <button className='header-demo-btn fw-medium text-white'>Get a Demo</button>
+                            </div>
+                        </div>
+                        <div className="col-12 col-lg-6">
+                            <img className='w-100 max-w-585' src="./src/assets/png/header-img.png" alt="header-img" />
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
